e2eaa95696a9177257c176973b142639f6f55cf7
Charles DeBrosse <cmdebrosse@outlook.com?>
2021-12-12 21:17:45 -0500

    finished HTML, started JS

--- script.js
+++ script.js
@@ -1,2 +1,14 @@
 const apiKey = "appid=b44dd980af0d1b9b47081f85d6567370";
-const unit = "units=imperial";
+var searchName = document.getElementById("search-name");
+var searchedCities = document.getElementById("previous-searches");
+
+var getWeatherData = function(searchCity) {
+  // format openweathermap api url
+  var apiURL = "https://api.openweathermap.org/data/2.5/weather?q=" + searchCity + "&" + apiKey + "&units=imperial";
+
+  fetch(apiURL).then(function(response) {
+    response.json().then(function(data) {
+      console.log(data);
+    });
+  });
+};
