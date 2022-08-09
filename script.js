a3ec41d40c393d5b17dd7931ebbb4515c3ef51da
Charles DeBrosse <cmdebrosse@outlook.com?>
2022-08-08 21:29:33 -0400

    current weather data displays for hardcoded city

--- script.js
+++ script.js
@@ -1,14 +1,58 @@
-const apiKey = "appid=b44dd980af0d1b9b47081f85d6567370";
-var searchName = document.getElementById("search-name");
-var searchedCities = document.getElementById("previous-searches");
+const apiKey = "&appid=cb106dd875c5a574b95cd0454390936e";
+const currentDate = new Date();
+const currentWeatherURL = "https://api.openweathermap.org/data/2.5/weather?q=";
+const forecastWeatherURL = "https://api.openweathermap.org/data/3.0/onecall?";
+const weatherIconURL = "http://openweathermap.org/img/wn/";
 
-var getWeatherData = function(searchCity) {
-  // format openweathermap api url
-  var apiURL = "https://api.openweathermap.org/data/2.5/weather?q=" + searchCity + "&" + apiKey + "&units=imperial";
+async function getWeather(cityName) {
+  const res = await fetch(
+    `${currentWeatherURL}${cityName}${apiKey}&units=imperial`
+  );
+  const currentData = await res.json();
+  const { lon, lat } = currentData.coord;
 
-  fetch(apiURL).then(function(response) {
-    response.json().then(function(data) {
-      console.log(data);
-    });
-  });
-};
+  async function getForecastWeather(lon, lat) {
+    const res = await fetch(
+      `${forecastWeatherURL}lat=${lat}&lon=${lon}${apiKey}&units=imperial`
+    );
+    const forecastData = await res.json();
+    console.log(forecastData);
+
+    if (forecastData.daily[0].uvi < 3) {
+      document.getElementById("currentUVI").textContent =
+        forecastData.daily[0].uvi;
+      document
+        .getElementById("currentUVI")
+        .classList.add("rounded", "shadow", "p-1", "bg-success", "text-white");
+    } else if (forecastData.daily[0].uvi < 8) {
+      document.getElementById("currentUVI").textContent =
+        forecastData.daily[0].uvi;
+      document
+        .getElementById("currentUVI")
+        .classList.add("rounded", "shadow", "p-1", "bg-warning", "text-white");
+    } else {
+      document.getElementById("currentUVI").textContent =
+        forecastData.daily[0].uvi;
+      document
+        .getElementById("currentUVI")
+        .classList.add("rounded", "shadow", "p-1", "bg-danger", "text-white");
+    }
+
+    const weatherIcon =
+      weatherIconURL + forecastData.current.weather[0].icon + "@2x.png";
+    console.log(weatherIcon);
+    document.getElementById("currentWeatherIcon").src = weatherIcon;
+  }
+  // console.log(currentData);
+  getForecastWeather(lon, lat);
+
+  document.getElementById("cityName").textContent = cityName;
+  document.getElementById("todaysDate").textContent =
+    currentDate.toLocaleDateString();
+  document.getElementById("currentTemp").textContent = currentData.main.temp;
+  document.getElementById("currentWind").textContent = currentData.wind.speed;
+  document.getElementById("currentHumidity").textContent =
+    currentData.main.humidity;
+}
+
+getWeather("Hawks");
